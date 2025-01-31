ee9ed43d8e1d17a9473f65017e3d3c90bf3519ec
Francisco-Teixeirax86 <franjateixeira@gmail.com>
2025-01-31 13:52:21 +0000

    Fixed bugs, now endpoints are accesed
    Fixed two major bugs that prevented access:
    First the fetch type of the roles of the user was lazy instead of eager, what was I thinking
    Second when I used lombok to auto inject my service into the product controller I forgot the final keyword.... kids read the documentation before you use a library :)

--- productMicroService/src/main/java/francisco/personal/productmicroservice/controllers/ProductController.java
+++ productMicroService/src/main/java/francisco/personal/productmicroservice/controllers/ProductController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
